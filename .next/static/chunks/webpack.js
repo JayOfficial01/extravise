507ef17e1a1fbb8a3eac6fc43a9745de1849862a
JayOfficial01 <jahangirjay5@gmail.com>
2022-11-13 17:39:45 +0500

    Fixes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "824f4152309a6847"; }
+/******/ 		__webpack_require__.h = function() { return "e4f7ecc6269fb433"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
